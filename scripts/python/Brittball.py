b30b59e438f9496c94a7fcbeb630233d64319cb9
Brittney Sinq <b.sinq@me.com>
2013-05-21 14:37:33 -0500

    Jake my first commit!

---
+++ scripts/python/Brittball.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# Copyright 2013 Jake Basile and Brittney Sinq
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+
+print(random.choice(list({
+      'I don\'t know what that means.',
+      'WHY????',
+      'Stand Up!',
+      'Reprioritize!',
+      'Has anyone seen Mickey?',
+      'Your nerd is showing.',
+      'I do what I want.',
+      'It\'s in the backlog',
+      'That sounds "terribad"',
+      'How does that make any sense?',
+      'We all know they can\'t do math',
+      'Quit trolling Jake',
+      'Not Helpful',
+      'Python is a snake right?',
+      'My dog is smarter than your kid',
+      'Push to Production',
+      'Have we merged trunk to master yet?',
+      'Ummmm... No.',
+      'Go Utes',
+      'Thats a real thing!'
+})))
